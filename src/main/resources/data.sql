2965fc70013cb9f9d3bcd8ca5c8035d3ce20d58e
s.limanov <imajing2010@yandex.ru>
2025-11-01 15:42:49 +0400

    add tests v2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,16 @@
-INSERT INTO authors (name) VALUES ('Фёдор Достоевский'), ('Лев Толстой');
-INSERT INTO genres (name) VALUES ('Роман'), ('Детектив');
+-- authors
+INSERT INTO authors (name) VALUES
+  ('Фёдор Достоевский'),
+  ('Лев Толстой'),
+  ('Агата Кристи');
+
+-- genres
+INSERT INTO genres (name) VALUES
+  ('Роман'),
+  ('Детектив');
+
+-- books
 INSERT INTO books (title, author_id, genre_id) VALUES
   ('Преступление и наказание', 1, 1),
-  ('Анна Каренина', 2, 1);
+  ('Анна Каренина', 2, 1),
+  ('Убийство в Восточном экспрессе', 3, 2);
