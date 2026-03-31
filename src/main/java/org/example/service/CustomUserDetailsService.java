99d26960af24d1afff3f14b1aa60b271c4c9b129
slimanov <imajing2010@yandex.ru>
2026-03-31 12:43:40 +0400

    add review improvement

--- src/main/java/org/example/service/CustomUserDetailsService.java
+++ src/main/java/org/example/service/CustomUserDetailsService.java
@@ -1,13 +1,10 @@
 package org.example.service;
 
 import org.example.model.User;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -19,14 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findByUsername(username)
+        return userService.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .authorities(Collections.singletonList(new SimpleGrantedAuthority(user.getRole())))
-                .disabled(!user.isEnabled())
-                .build();
     }
 }
