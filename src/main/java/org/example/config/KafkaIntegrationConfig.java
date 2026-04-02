dcc93fa44db5b1b475c89db38f43da81de084616
slimanov <imajing2010@yandex.ru>
2026-04-02 18:37:12 +0400

    add kafka2

--- src/main/java/org/example/config/KafkaIntegrationConfig.java
+++ src/main/java/org/example/config/KafkaIntegrationConfig.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -30,9 +28,6 @@ public class KafkaIntegrationConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${kafka.topics.author-response}")
-    private String authorResponseTopic;
-
     @Value("${spring.kafka.consumer.group-id}")
     private String consumerGroupId;
 
@@ -45,7 +40,7 @@ public class KafkaIntegrationConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        
+
         return new DefaultKafkaProducerFactory<>(props);
     }
 
@@ -67,34 +62,17 @@ public class KafkaIntegrationConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, AuthorResponse.class.getName());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        
+
+        // Создаем JsonDeserializer с отключенным type mapping
+        JsonDeserializer<AuthorResponse> deserializer = new JsonDeserializer<>(AuthorResponse.class);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeHeaders(false); // Отключаем использование type headers из Kafka
+
         return new DefaultKafkaConsumerFactory<>(
-            props,
-            new StringDeserializer(),
-            new JsonDeserializer<>(AuthorResponse.class)
-        );
-    }
-
-    /**
-     * Kafka Template для получения AuthorResponse
-     */
-    @Bean
-    public KafkaTemplate<String, AuthorResponse> authorResponseKafkaTemplate() {
-        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(new HashMap<>()));
-    }
-
-    /**
-     * Listener Container для получения ответов из Kafka
-     */
-    @Bean
-    public KafkaMessageListenerContainer<String, AuthorResponse> authorResponseListenerContainer() {
-        ContainerProperties containerProps = new ContainerProperties(authorResponseTopic);
-        return new KafkaMessageListenerContainer<>(
-            authorResponseConsumerFactory(),
-            containerProps
+                props,
+                new StringDeserializer(),
+                deserializer
         );
     }
 
@@ -104,7 +82,7 @@ public class KafkaIntegrationConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, AuthorResponse> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, AuthorResponse> factory =
-            new ConcurrentKafkaListenerContainerFactory<>();
+                new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(authorResponseConsumerFactory());
         return factory;
     }
